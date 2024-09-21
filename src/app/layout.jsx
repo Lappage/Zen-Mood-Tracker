7158865c5515d123214ed51f11da8a6e7e032028
Lee Lappage <114675964+Lappage@users.noreply.github.com>
2024-09-21 15:24:35 +0100

    init

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,34 @@
+import Head from "next/head";
+import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
 
+const openSans = Open_Sans({ subsets: ["latin"] });
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
+
 export const metadata = {
-  title: "Mood Tracker",
+  title: "Zen",
   description: "Track your mood, every day of the year!",
 };
 
 export default function RootLayout({ children }) {
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={`text-base sm:text-lg textGradient ${fugaz.className}`}>Zen</h1>
+    </header>
+  );
+
+  const footer = (
+    <footer className="p-4 sm:p-8 grid place-items-center">
+      <p className={`text-indigo-500 ${fugaz.className}`}>This project is open source.</p>
+    </footer>
+  );
   return (
     <html lang="en">
-      <body className={``}>{children}</body>
+      <body className={`w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col text-slate-800 ${openSans.className}`}>
+        {header}
+        {children}
+        {footer}
+      </body>
     </html>
   );
 }
